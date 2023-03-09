3bdbf8f4b3ccdfa8a31d594f2428594a351f7889
Peter Nelson <pnelson@sudscreative.com>
2023-03-08 21:34:49 -0700

    We win

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Moody gonna love dis" />
+      <HelloWorld msg="Bob is also gonna love dis" />
     </div>
   </header>
 
